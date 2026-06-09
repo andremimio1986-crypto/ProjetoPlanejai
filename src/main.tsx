2e22b8a646f1f3887872139e5bbe9911cbaf1f21
André Luiz <andremimio1986@gmail.com>
2026-06-09 17:59:48 -0300

    Cria a estrutura base da página de formulário de simulação

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
-import { ThemeProvider } from './contexts/theme/ThemeContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<ThemeProvider>
-			<App />
-		</ThemeProvider>
+		<App />
 	</StrictMode>
 );
